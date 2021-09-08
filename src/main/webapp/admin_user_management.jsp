4692bed03c9535b8850edbf460f54240234eb479
ronron18 <ronankonstantin@gmail.com>
2021-09-08 21:40:26 +1000

    pain and suffering

--- src/main/webapp/admin_user_management.jsp
+++ src/main/webapp/admin_user_management.jsp
@@ -1,9 +1,22 @@
 
+<%@page import="uts.asd.model.*"%>
+<%@page import="uts.asd.controller.*"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
 <html>
     <head>
         <jsp:include page="nav.jsp"/> 
     </head>
     <body>
-
+        <div>
+                <table>
+                    <form action="AdminManagementServlet" method="GET">
+                        <tr>
+                            <td>E-mail address</td>
+                            <td><input type="text" placeholder="name@something.com" name="email"></td>
+                            <td><input type="submit" value="submit" class="button"></td>
+                        </tr>
+                    </form>
+                </table>
+        </div>
     </body>
 </html>
---
+++ src/uts/asd/controller/AdminManagementServlet.java
@@ -0,0 +1,50 @@
+package uts.asd.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import uts.asd.model.User;
+import uts.asd.model.dao.DBManager;
+
+public class AdminManagementServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Validator validator = new Validator();
+        String email = request.getParameter("email");
+        DBManager manager = (DBManager) session.getAttribute("manager");
+        ArrayList<User> userList = null;
+
+        try {
+            userList = manager.fetchAllUsers();
+            
+        } catch (SQLException | NullPointerException ex) {
+
+        }
+
+        if(email == null || email.isEmpty()) {
+            session.setAttribute("userList", userList);
+            request.getRequestDispatcher("admin_user_management.jsp").include(request, response);
+        }
+        else {
+            try {
+                userList = manager.fetchUsersEmail(email);
+                } 
+                catch (SQLException | NullPointerException ex) {
+
+                }
+                
+                session.setAttribute("userList", userList);
+                request.getRequestDispatcher("admin_user_management.jsp").include(request, response);
+        }
+
+
+    }
+}
