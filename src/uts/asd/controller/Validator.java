f1672c72886a3d5f1cfca63a007a05c11b9d80a9
leonwangg1 <leonwang148@gmail.com>
2021-10-02 22:27:46 +1000

    fixed email pattern

--- src/uts/asd/controller/Validator.java
+++ src/uts/asd/controller/Validator.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 import javax.servlet.http.HttpSession;
 
 public class Validator implements Serializable {
-    private String emailPattern = "[a-zA-Z0-9]{3,}"; //^(.+)@(.+)$
+    private String emailPattern = "^(.+)@(.+)$"; //^(.+)@(.+)$
     private String namePattern = "[a-zA-Z0-9]{3,}";
     private String passwordPattern = "^[a-zA-Z0-9!@#$&]{4,}";
     private String phonePattern = "([0]{1}[4]{1}[0-9]{8})*";
