0a04485e2ce2b2290fee7947ff9f92fa9579b568
iMystic <clarence.e.cheung@gmail.com>
2021-10-05 04:08:52 +1100

    added cancel booking and some test cases

---
+++ src/uts/asd/controller/CancelBookingServlet.java
@@ -0,0 +1,32 @@
+package uts.asd.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import uts.asd.model.*;
+import uts.asd.model.dao.*;
+import java.util.*;
+
+public class CancelBookingServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        RoomDBManager manager = (RoomDBManager) session.getAttribute("room");
+        try {
+            int bookingID = Integer.parseInt(request.getParameter("bookingID"));
+            manager.cancelBooking(bookingID);
+            int userID = ((User)session.getAttribute("user")).getId();;
+            ArrayList<Booking> bookings = manager.listBookingByUserID(userID);
+            session.setAttribute("bookings", bookings);
+            request.getRequestDispatcher("listBookings.jsp").include(request, response);
+            
+        }
+        catch (Exception e) {
+            
+        }
+    }
+}
--- test/ClarenceTest.java
+++ test/ClarenceTest.java
@@ -1,11 +1,18 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import uts.asd.model.*;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import java.sql.*;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.*;
 
-class ClarenceTest {
+
+public class ClarenceTest {
 
     @Test
-    void roomTypeTest() {
+    public void roomTypeTest() {
         int roomTypeID = 1;
         double cost = 99.99;
         int numBeds = 2;
@@ -17,6 +24,32 @@ class ClarenceTest {
         assertEquals(roomType.getNumBeds(), numBeds);
         assertEquals(roomType.getSuite(), suite);
         assertEquals(roomType.getDescription(), description);
+    }
+
+    @Test
+    public void bookingTest() {
+        int bookingID = 123;
+        int userID = 15;
+        int roomID = 150;
+        String startingDate = "05/10/2021";
+        String endingDate = "10/10/2021";
+        String status = "Booked";
+        Boolean paid = false;
+        double totalCost = 99.99;
+        Booking booking = new Booking(bookingID, userID, roomID, startingDate, endingDate, status, paid, totalCost);
+        assertEquals(booking.getBookingID(), bookingID);
+        assertEquals(booking.getUserID(), userID);
+        assertEquals(booking.getRoomID(), roomID);
+        assertEquals(booking.getStatus(), status);
+        assertEquals(booking.getPaid(), paid);
+        assertEquals(booking.getTotalCost(), totalCost);
+    }
+
+    @Test
+    public void differenceInDaysTest() {
+        LocalDate today = LocalDate.now();
+        LocalDate tomorrow = LocalDate.now().plusDays(1);
+        assertEquals(ChronoUnit.DAYS.between(today, tomorrow), 1);
     }
 
 }
