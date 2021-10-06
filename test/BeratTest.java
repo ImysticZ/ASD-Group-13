b4cbc2153a102f577d7fa769ac617cbd361081bf
Berat Appak <Bappak1@hotmail.com>
2021-10-06 13:39:09 +1100

    hopefully fixed unit test

--- test/BeratTest.java
+++ test/BeratTest.java
@@ -1,10 +1,8 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
-import org.junit.Test;
 import uts.asd.model.*;
 import uts.asd.model.dao.DBConnector;
 import uts.asd.model.dao.testPaymentDB;
@@ -16,8 +14,6 @@ public class BeratTest {
 
     public BeratTest() throws ClassNotFoundException, SQLException {
         dbConn = new DBConnector();
-        conn = dbConn.openConnection();
-        testPayment = new testPaymentDB(conn);
     }
 
     @Test
@@ -33,18 +29,9 @@ public class BeratTest {
     }
 
     @Test
-    public void testCheckInPatron() throws SQLException {
-        Booking expectedBooking= null; //The expected Booking object
-        Booking actualBooking= null; //The actual Booking object
-        expectedBooking = new Booking(31, 1012, 2, "2021-10-14", "2021-10-21", "Booked", true, 199.50); //create the actual expected booking
-        expectedBooking.setStatus("Checked In"); //set the Status of the EXPECTED BOOKING to 'Checked In'
-        testPayment.testCheckInPatron("Checked In"); //update the ACTUAL BOOKING status to 'Checked In' using DB Operations
-        actualBooking = testPayment.returnBooking(); //Return the ACTUAL BOOKING to actual booking object
-        assertEquals(expectedBooking.getStatus(), actualBooking.getStatus()); //Check whether expected status and actual status are the same ('Checked In')
-    }
-
-    @Test
     public void testCheckOutPatron() throws SQLException {
+        conn = dbConn.openConnection();
+        testPayment = new testPaymentDB(conn);
         Booking expectedBooking= null; //The expected Booking object
         Booking actualBooking= null; //The actual Booking object
         expectedBooking = new Booking(31, 1012, 2, "2021-10-14", "2021-10-21", "Booked", true, 199.50); //create the actual expected booking
@@ -52,13 +39,17 @@ public class BeratTest {
         testPayment.testCheckInPatron("Checked Out"); //update the ACTUAL BOOKING status to 'Checked out' using DB Operations
         actualBooking = testPayment.returnBooking(); //Return the ACTUAL BOOKING to actual booking object
         assertEquals(expectedBooking.getStatus(), actualBooking.getStatus()); //Check whether expected status and actual status are the same ('Checked Out')
+        dbConn.closeConnection();
     }
 
     @Test
     public void testFetchBookings() throws SQLException {
+        conn = dbConn.openConnection();
+        testPayment = new testPaymentDB(conn);
         ArrayList<Booking> allBooking = null; //instantiate Array of Bookings
         allBooking = testPayment.fetchBooking(); //fetches All Bookings in database and store in allBooking
         int noBookings = testPayment.countBookings(); //stores the number of Bookings in database 
         assertEquals(noBookings, allBooking.size()); //Check whether the number of booking is the same as the size of the Booking ArrayList
+        dbConn.closeConnection();
     }
 }
