2ea553d568597febf625efd5f484a6868d573b90
iMystic <clarence.e.cheung@gmail.com>
2021-10-07 21:51:29 +1100

    changed db

--- src/uts/asd/model/dao/DB.java
+++ src/uts/asd/model/dao/DB.java
@@ -3,7 +3,7 @@ package uts.asd.model.dao;
 import java.sql.Connection;
 
 public abstract class DB {
-    String url = "jdbc:mysql://bc2dyro2kdvcc2jmmd9e-mysql.services.clever-cloud.com:3306/bc2dyro2kdvcc2jmmd9e";
+    String url = "jdbc:mysql://bc2dyro2kdvcc2jmmd9e-mysql.services.clever-cloud.com:3306/bc2dyro2kdvcc2jmmd9e?autoReconnect=true";
     String dbuser = "usws9urc96uqn2aw";
     String dbpass = "aOuX54759girLCF7QIkY";
     String driver = "com.mysql.cj.jdbc.Driver"; //jdbc client driver - built in with NetBeans   
